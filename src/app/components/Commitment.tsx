2fb8a496ff2f3de06d6caf965c36b53e8bccbf3c
rahulgagrai25 <rahulgagraicit369@gmail.com>
2025-08-31 23:37:59 +0530

    Added Commit Section

--- src/app/components/Commitment.tsx
+++ src/app/components/Commitment.tsx
@@ -3,13 +3,23 @@ import React from "react";
 function Commitment() {
   return (
     <div className="commitment">
-      <div className="div1">
-        <img src="/commit/1.png" alt="" /> </div>
-      <div className="div2"> 
+      <div className="commit-info div1">
+        <img src="/commit/1.png" alt="" /> 
+        <h1>WINNING AWARD LAW FIRM</h1>
+        <p>Lawyers and attorneys act as both legal advisors and advocates, guiding individuals and businesses through intricate legal processes.</p>
+        <a href="">LEARN MORE →</a>
+        </div>
+      <div className="commit-info div2"> 
         <img src="/commit/2.png" alt="" />
+        <h1>CONFIDENTIAL</h1>
+        <p>Your information is held in strict confidence with us.We prioritize safeguarding your sensitive data. Rest assured, your case details remain private.</p>
+        <a href="">LEARN MORE →</a>
       </div>
-      <div className="div3"> 
+      <div className="commit-info div3"> 
         <img src="/commit/3.png" alt="" />
+        <h1>LEGAL PROTECTION</h1>
+        <p>We provide robust legal protection for our clients. Your rights and interests are our top priority. Rest assured for safeguarding your legal rights.</p>
+        <a href="">LEARN MORE →</a>      
       </div>
     </div>
   );
